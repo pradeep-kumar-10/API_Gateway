a8f009a57f4ee6047a9fbc5b6859867a0429fb6e
Pradeep Kumar <pra45516@gmail.com>
2024-05-23 12:03:15 +0530

    Added rate limiter & axios

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const ratelimit = require('express-rate-limit');
+const axios = require('axios');
 
 const app = express();
 
 const PORT = 3005;
 
+const limiter = ratelimit({
+    windowMS: 2*60*1000, //2min
+    max:5
+});
+
 app.use(morgan('combined'));
+app.use(limiter);
+app.use('/bookingservice', async(req,res, next) => {
+    console.log('Hi');
+    console.log(req.headers['x-access-token']);
+    try{
+        const response = await axios.get('http://localhost:3001/api/v1/isauthenticated', {
+            headers: {
+                'x-access-token': req.headers['x-access-token']
+            }
+        });
+        console.log(response.data);
+
+        if(response.data.success) {
+            next();
+        } else {
+            return res.status(401).json({
+                message: 'Unauthorised'
+            })
+        }
+    } catch(error) {
+        return res.status(401).json({
+            message: 'Unauthorised'
+        });
+    }
+})
 app.use('/bookingservice', createProxyMiddleware({
     target: 'http://localhost:3002/bookingservice/',
     changeOrigin : true
