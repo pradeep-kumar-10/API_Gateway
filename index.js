4d155ca66fe721f39ac9822bbe5d421af92854ec
Pradeep Kumar <pra45516@gmail.com>
2024-05-23 11:32:58 +0530

    Added proxy request to booking service

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
 const PORT = 3005;
 
 app.use(morgan('combined'));
+app.use('/bookingservice', createProxyMiddleware({
+    target: 'http://localhost:3002/bookingservice/',
+    changeOrigin : true
+}))
 
 app.get('/home', (req,res) => {
     return res.json({message: 'OK'});
